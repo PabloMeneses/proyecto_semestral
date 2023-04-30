202b9d78df96e0081bc91d02c217244427accbcf
PabloMeneses <pa.meneses@duocuc.cl>
2023-04-30 17:03:07 -0400

    validación formulario contacto

--- ConfeccionesAS/js/formcontacto.js
+++ ConfeccionesAS/js/formcontacto.js
@@ -1,41 +1,54 @@
-var nombre = document.getElementById("name");
-var correo = document.getElementById("email");
-var clave = document.getElementById("password");
+const form = document.getElementById('form');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const telefono = document.getElementById('telefono');
+const message = document.getElementById('message');
+const warnings = document.getElementById('warnings');
+const namesList = ['Juan', 'Pedro', 'Ana', 'Maria'];
 
-const formulario = document.getElementById("form");
-var msj = document.getElementById("warnings");
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  let isValid = true;
+  let warningMessage = '';
 
-formulario.addEventListener('submit',e =>{
-    e.preventDefault();
-    let msjMostrar ="";
-    let enviar = false;
-    if(nombre.value.length < 4 || nombre.value.length > 10 ){
-        msjMostrar = msjMostrar + "El nombre debe tener entre 4 y 10 caracteres";
-        enviar = true;
-    }
-    var letra = nombre.value.charAt(0);
-    if(!esMayuscula(letra)){
-        msjMostrar += "<br>El nombre debe comenzar con mayúscula";
-        enviar = true;
-    }
+  if (name.value === '' || name.value == null) {
+    isValid = false;
+    warningMessage += 'Por favor ingrese su nombre\n';
+  } else if (!/^[A-Z][a-z]{2,9}$/.test(name.value)) {
+    isValid = false;
+    warningMessage += 'Por favor ingrese un nombre válido con la primera letra en mayúscula y entre 3 y 10 caracteres\n';
+  } else if (namesList.includes(name.value)) {
+    isValid = false;
+    warningMessage += 'El nombre ingresado ya existe, por favor ingrese otro\n';
+  }
 
-    if(nombre.value == 'Victor'){
-        msjMostrar += "El nombre no puede ser Victor";
-        enviar = true;
-    }
+  if (email.value === '' || email.value == null) {
+    isValid = false;
+    warningMessage += 'Por favor ingrese su dirección de correo electrónico\n';
+  } else if (!/\S+@\S+\.\S+/.test(email.value)) {
+    isValid = false;
+    warningMessage += 'Por favor ingrese una dirección de correo electrónico válida\n';
+  }
 
-    if(enviar){
-        msj.innerHTML = msjMostrar;
-    }
-    else
-        msj.innerHTML = "Enviado";
+  if (telefono.value === '' || telefono.value == null) {
+    isValid = false;
+    warningMessage += 'Por favor ingrese su número de teléfono\n';
+  } else if (!/^\d{9}$/.test(telefono.value)) {
+    isValid = false;
+    warningMessage += 'Por favor ingrese un número de teléfono válido de 9 dígitos\n';
+  }
+
+  if (message.value === '' || message.value == null) {
+    isValid = false;
+    warningMessage += 'Por favor ingrese su mensaje\n';
+  } else if (message.value.split(' ').length < 5) {
+    isValid = false;
+    warningMessage += 'El mensaje debe contener al menos 5 palabras\n';
+  }
+
+  if (isValid) {
+    form.submit();
+  } else {
+    warnings.innerText = warningMessage;
+  }
 });
-
-function esMayuscula(letra){
-    if(letra == letra.toUpperCase()){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
