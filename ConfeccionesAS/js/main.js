ab154e5f908ab25941a8e43993172186db5e9c56
BasFG <bas.fuentesg@duocuc.cl>
2023-04-30 16:45:42 -0400

    Cambios Importantes
    VALIDACIONES Y PARTES DEL USUARIO

--- ConfeccionesAS/js/main.js
+++ ConfeccionesAS/js/main.js
@@ -18,7 +18,7 @@ window.addEventListener('scroll', function () {
         nav.classList.remove('nav-fixed')
     }
 })
- // script del menu responsive effecto accordeon
+// script del menu responsive effecto accordeon
 var submenu = document.getElementsByClassName('link-submenu')
 
 for (var i = 0; i < submenu.length; i++) {
@@ -89,14 +89,86 @@ const sendButton = document.querySelector('#send-button');
 const emailInput = document.querySelector('#email-input');
 
 sendButton.addEventListener('click', (event) => {
-  event.preventDefault(); // Prevenir que el formulario se envíe automáticamente
-  if (emailInput.checkValidity()) {
-    // Enviar solicitud
-    const messageElement = document.createElement('@');
-    messageElement.innerText = 'La solicitud fue realizada por favor revise su correo';
-    document.body.appendChild(messageElement);
-  } else {
-    // Mostrar mensaje de error
-    alert('Por favor ingresa un email válido');
-  }
+    event.preventDefault(); // Prevenir que el formulario se envíe automáticamente
+    if (emailInput.checkValidity()) {
+        // Enviar solicitud
+        const messageElement = document.createElement('@');
+        messageElement.innerText = 'La solicitud fue realizada por favor revise su correo';
+        document.body.appendChild(messageElement);
+    } else {
+        // Mostrar mensaje de error
+        alert('Por favor ingresa un email válido');
+    }
 });
+
+<script>
+$(document).ready(function () {
+    $('#register-button').click(function (e) {
+        e.preventDefault();
+
+        var name = $('#name-input').val();
+        var surname = $('#surname-input').val();
+        var email = $('#email-input').val();
+        var phone = $('#phone-input').val();
+        var password = $('#password-input').val();
+        var confirmPassword = $('#confirm-password-input').val();
+
+        var nameRegex = /^[a-zA-Z]+$/;
+        var emailRegex = /^\S+@\S+\.\S+$/;
+        var phoneRegex = /^\d+$/;
+        var passwordRegex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+
+        if (name == '') {
+            alert('Por favor ingresa tu nombre.');
+            return false;
+        } else if (!nameRegex.test(name)) {
+            alert('El nombre solo puede contener letras.');
+            return false;
+        }
+
+        if (surname == '') {
+            alert('Por favor ingresa tu apellido.');
+            return false;
+        } else if (!nameRegex.test(surname)) {
+            alert('El apellido solo puede contener letras.');
+            return false;
+        }
+
+        if (email == '') {
+            alert('Por favor ingresa tu correo electrónico.');
+            return false;
+        } else if (!emailRegex.test(email)) {
+            alert('Ingresa un correo electrónico válido.');
+            return false;
+        }
+
+        if (phone == '') {
+            alert('Por favor ingresa tu número de teléfono.');
+            return false;
+        } else if (!phoneRegex.test(phone)) {
+            alert('El número de teléfono solo puede contener números.');
+            return false;
+        }
+
+        if (password == '') {
+            alert('Por favor ingresa tu contraseña.');
+            return false;
+        } else if (!passwordRegex.test(password)) {
+            alert('La contraseña debe contener al menos 8 caracteres, 1 número y 1 carácter especial.');
+            return false;
+        }
+
+        if (confirmPassword == '') {
+            alert('Por favor confirma tu contraseña.');
+            return false;
+        } else if (confirmPassword != password) {
+            alert('Las contraseñas no coinciden.');
+            return false;
+        }
+
+        // Si se llega a este punto, todos los campos están validados correctamente
+        alert('Formulario validado correctamente.');
+        return true;
+    });
+});
+</script>
